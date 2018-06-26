1b23f7d79cbcb85deed5db9207db14940c1a9f75
LiuJP <frankpi@users.noreply.github.com>
2018-06-26 14:15:11 +0800

    Add files via upload

--- jsExcel.js
+++ jsExcel.js
@@ -1,4 +1,4 @@
-
+// npm install xlsx -save
 var xl =require('xlsx');
 //workbook 对象，指的是整份 Excel 文档。我们在使用 js-xlsx 读取 Excel 文档之后就会获得 workbook 对象。
 var workbook =  xl.readFile("/Users/ljp/Downloads/海外mod.xlsx")
